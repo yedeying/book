0d876f66f6db7b11a70f1ec7774bbf12585edc17
yedeying999 <kanwode918@qq.com>
2014-06-06 19:32:02 +0800

    20140606

--- public/js/list.js
+++ public/js/list.js
@@ -1,50 +1,6 @@
 window.onload = function() {
-
-  // initial for tablist
-  (function() {
-    var listIcon = document.querySelector('.tablist-icon');
-    var list = document.querySelector('.tablist');
-    var lis = document.querySelectorAll('.tablist-item');
-    var fa = document.querySelector('.tablist-fa');
-    list.style.left = '-' + list.offsetWidth + 'px';
-    listIcon.onclick = listOut;
-    listIcon.style.top = lis[3].offsetTop + 'px';
-
-    function listOut() {
-      listIcon.style.left = list.offsetWidth + 'px';
-      listIcon.style.opacity = '.8';
-      list.style.left = '0';
-      console.log(fa.classList);
-      fa.classList.toggle('fa-th-list');
-      fa.classList.toggle('fa-angle-left');
-      listIcon.onclick = listIn;
-    }
-
-    function listIn() {
-      listIcon.style.left = '0';
-      listIcon.style.opacity = '.5';
-      list.style.left = '-' + list.offsetWidth + 'px';
-      fa.classList.toggle('fa-th-list');
-      fa.classList.toggle('fa-angle-left');
-      listIcon.onclick = listOut;
-    }
-  })();
-
-  // initial for search
-  (function() {
-    var searchBtn = document.querySelector('.btn-search');
-    var searchBox = document.querySelector('.search-box');
-    var section = document.querySelector('.search-box + section');
-    searchBtn.onclick = function() {
-      var cla = searchBtn.classList;
-      if(cla.contains('active')) {
-        searchBox.style.top = '7px';
-        section.style.marginTop = '0px';
-      } else {
-        searchBox.style.top = '61px';
-        section.style.marginTop = '54px';
-      }
-      cla.toggle('active');
-    };
-  })();
+  initTablist();
+  initSearch();
+  initNav();
+  initLink();
 };
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,56 @@
 var express = require('express');
+var Page = require('../module/Page');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res) {
-  res.render('index', {type: 'index'});
+  var p = new Page('index');
+  p.render(function(err, data) {
+    if(err) throw err;
+    res.render('index', {type: 'index', data: data});
+  });
 });
 
+/* GET list page */
 function renderList(str) {
   var type = str;
   function render(req, res) {
-    res.render('list', {type: str});
+    var p = new Page('list');
+    var id = 1;
+    if(req.params && req.params.id) id = req.params.id;
+    p.render({type: type, page: id}, function(err, data) {
+      if(err) throw err;
+      res.render('list', {type: str, data: data, page: id});
+    });
   }
   return render;
 }
+
 router.get('/list', renderList('list'));
 router.get('/recommand', renderList('recommand'));
 router.get('/special', renderList('special'));
 router.get('/hot', renderList('hot'));
 router.get('/new', renderList('new'));
-router.get('/catagory/:id', function(req, res) {
-  res.render('list', {type: 'catagory', id: req.params.id + 1});
+router.get('/list/:id', renderList('list'));
+router.get('/recommand/:id', renderList('recommand'));
+router.get('/special/:id', renderList('special'));
+router.get('/hot/:id', renderList('hot'));
+router.get('/new/:id', renderList('new'));
+
+/* GET catagory list page */
+function renderCatagoryList(req, res) {
+  var p = new Page('catagory');
+  p.render({id: req.params.id}, function(err, data) {
+    if(err) throw err;
+    res.render('catagory', {type: 'catagory'}, data: data);
+  });
+}
+router.get('/catagory/:id', renderCatagoryList);
+router.get('/catagory/:id/:page', renderCatagoryList);
+
+/* GET detail page */
+router.get('/detail/:id', function(req, res) {
+  res.render('detail', {type: 'detail', id: req.params.id});
 });
 
 module.exports = router;
