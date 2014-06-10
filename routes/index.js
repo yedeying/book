b953410d0f003a1a238ec1a5246c200b67a5fb02
yedeying999 <kanwode918@qq.com>
2014-06-10 10:26:17 +0800

    20140610

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var Page = require('../module/Page');
 var router = express.Router();
+var isLogin = false;
 
 /* GET home page */
 router.get('/', function(req, res) {
   var p = new Page('index');
   p.render(function(err, data) {
     if(err) throw err;
-    res.render('index', {type: 'index', data: data});
+    res.render('index', {
+      isLogin: isLogin,
+      type: 'index',
+      data: data
+    });
   });
 });
 
@@ -19,9 +24,17 @@ router.get('/', function(req, res) {
       var p = new Page('list');
       var page = 1;
       if(req.params && req.params.page) page = req.params.page;
-      p.render({type: type, page: page}, function(err, data) {
+      p.render({
+        type: type,
+        page: page
+      }, function(err, data) {
         if(err) throw err;
-        res.render('list', {type: str, data: data, page: page});
+        res.render('list', {
+          isLogin: isLogin,
+          type: str,
+          data: data,
+          page: page
+        });
       });
     }
     return render;
@@ -47,9 +60,18 @@ router.get('/', function(req, res) {
     var id = 1;
     if(req.params && req.params.id) id = req.params.id;
     if(req.params && req.params.page) page = req.params.page;
-    p.render({type: 'catagory', page: page, id: id}, function(err, data) {
+    p.render({type: 'catagory',
+      page: page,
+      id: id
+    }, function(err, data) {
       if(err) throw err;
-      res.render('list', {type: 'catagory', id: id, data: data, page: page});
+      res.render('list', {
+        isLogin: isLogin,
+        type: 'catagory',
+        id: id,
+        data: data,
+        page: page
+      });
     });
   }
   router.get('/catagory/:id', renderCatagoryList);
@@ -62,9 +84,53 @@ router.get('/', function(req, res) {
     var p = new Page('detail');
     var id = 0;
     if(req.params && req.params.id) id = req.params.id;
-    p.render({type: 'detail', id: id}, function(err, data) {
+    p.render({
+      type: 'detail',
+      id: id
+    }, function(err, data) {
       if(err) throw err;
-      res.render('detail', {type: 'detail', id: id, data: data});
+      res.render('detail', {
+        isLogin: isLogin,
+        type: 'detail',
+        id: id,
+        data: data
+      });
+    });
+  });
+})();
+
+/* GET cart page */
+// (function(){
+//   router.get('/cart', function(req, res) {
+//     res.render('cart', {type: 'cart'});
+//   });
+// })();
+
+(function(){
+  router.get('/cart', function render(req, res) {
+    var p = new Page('list');
+    var page = 1;
+    p.render({
+      type: 'hot',
+      page: page
+    }, function(err, data) {
+      if(err) throw err;
+      res.render('cart', {
+        isLogin: isLogin,
+        type: 'cart',
+        data: data,
+        page: page
+      });
+    });
+  });
+})();
+
+/* GET login page */
+(function(){
+  router.get('/login', function(req, res) {
+    res.render('login', {
+      isLogin: isLogin,
+      type: 'login'
     });
   });
 })();
